9d43302e851d30f29a7321eda818d666f9246692
Tenzija <48459321+tenzija@users.noreply.github.com>
2022-12-06 15:29:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import store from './store/store';
@@ -13,8 +12,3 @@ root.render(
     <App />
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
