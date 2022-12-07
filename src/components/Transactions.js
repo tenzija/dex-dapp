e1975796b45fc67fd06cceaca73db48cb46fee84
Tenzija <48459321+tenzija@users.noreply.github.com>
2022-12-07 17:40:16 +0100

    Add files via upload

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,85 +1,113 @@
 import { useSelector } from "react-redux";
-import { myOpenOrdersSelector } from "../store/selectors";
+import { useRef, useState } from "react";
+import { 
+  myOpenOrdersSelector,
+  myFilledOrdersSelector
+} from "../store/selectors";
 
 import sort from '../assets/sort.svg'
 
 import Banner from "./Banner";
 
 const Transactions = () => {
+  const [showMyOrders, setShowMyOrders] = useState(true)
+
   const myOpenOrders = useSelector(myOpenOrdersSelector)
+  const myFilledOrders = useSelector(myFilledOrdersSelector)
   const symbols = useSelector(state => state.tokens.symbols)
+
+  const tradeRef = useRef(null)
+  const orderRef = useRef(null)
+
+  const tabHandler = (e) => {
+    if (e.target.className !== orderRef.current.className) {
+      e.target.className = 'tab tab--active'
+      orderRef.current.className = 'tab'
+      setShowMyOrders(false)
+    } else {
+      e.target.className = 'tab tab--active'
+      tradeRef.current.className = 'tab'
+      setShowMyOrders(true)
+    }
+  }
 
   return (
     <div className="component exchange__transactions">
-      <div>
-        <div className='component__header flex-between'>
-          <h2>My Orders:</h2>
+      {showMyOrders ? (
+        <div>
+          <div className='component__header flex-between'>
+            <h2>My Orders:</h2>
 
-          <div className='tabs'>
-            <button className='tab tab--active'>orders</button>
-            <button className='tab'>trades</button>
+            <div className='tabs'>
+              <button onClick={tabHandler} ref={orderRef} className='tab tab--active'>orders</button>
+              <button onClick={tabHandler} ref={tradeRef} className='tab'>trades</button>
+            </div>
           </div>
+            {!myOpenOrders || myOpenOrders.length === 0 ? (
+              <Banner text='No Open Orders' />
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>{symbols && symbols[0]}<img src={sort} alt='Sort' /></th>
+                    <th>{symbols && symbols[0]}/{symbols && symbols[1]}<img src={sort} alt='Sort' /></th>
+                    <th></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  
+                  {myOpenOrders && myOpenOrders.map((order, index) => {
+                    return(
+                      <tr key={index}>
+                        <td style={{ color:`${order.orderTypeClass}` }}>{order.token0Amount}</td>
+                        <td>{order.tokenPrice}</td>
+                        {/* <td> ADD CANCEL ORDER </td> */}
+                      </tr>
+                    )
+                  })}
+                  
+                </tbody>
+              </table>
+            )}
+            
+
         </div>
-          {!myOpenOrders || myOpenOrders.length === 0 ? (
-            <Banner text='No Open Orders' />
-          ) : (
-            <table>
-              <thead>
-                <tr>
-                  <th>{symbols && symbols[0]}<img src={sort} alt='Sort' /></th>
-                  <th>{symbols && symbols[0]}/{symbols && symbols[1]}<img src={sort} alt='Sort' /></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                
-                {myOpenOrders && myOpenOrders.map((order, index) => {
-                  return(
-                    <tr key={index}>
-                      <td style={{ color:`${order.orderTypeClass}` }}>{order.token0Amount}</td>
-                      <td>{order.tokenPrice}</td>
-                      {/* <td> ADD CANCEL ORDER </td> */}
-                    </tr>
-                  )
-                })}
-                
-              </tbody>
-            </table>
-          )}
-          
+      ) : (
+        <div>
+          <div className='component__header flex-between'>
+            <h2>My Transactions:</h2>
 
-      </div>
+            <div className='tabs'>
+              <button onClick={tabHandler} ref={orderRef} className='tab tab--active'>orders</button>
+              <button onClick={tabHandler} ref={tradeRef} className='tab'>trades</button>
+            </div>
+          </div>
 
-      {/* <div> */}
-        {/* <div className='component__header flex-between'> */}
-          {/* <h2>My Transactions</h2> */}
+          <table>
+            <thead>
+              <tr>
+                <th>Time<img src={sort} alt='Sort' /></th>
+                <th>{symbols && symbols[0]}<img src={sort} alt='Sort' /></th>
+                <th>{symbols && symbols[0]}/{symbols && symbols[1]}<img src={sort} alt='Sort' /></th>
+              </tr>
+            </thead>
+            <tbody>
 
-          {/* <div className='tabs'> */}
-            {/* <button className='tab tab--active'>Orders</button> */}
-            {/* <button className='tab'>Trades</button> */}
-          {/* </div> */}
-        {/* </div> */}
-
-        {/* <table> */}
-          {/* <thead> */}
-            {/* <tr> */}
-              {/* <th></th> */}
-              {/* <th></th> */}
-              {/* <th></th> */}
-            {/* </tr> */}
-          {/* </thead> */}
-          {/* <tbody> */}
-
-            {/* <tr> */}
-              {/* <td></td> */}
-              {/* <td></td> */}
-              {/* <td></td> */}
-            {/* </tr> */}
-
-          {/* </tbody> */}
-        {/* </table> */}
-
-      {/* </div> */}
+              {myFilledOrders && myFilledOrders.map((order, index) => {
+                return(
+                  <tr key={index}>
+                    <td>{order.formattedTimestamp}</td>
+                    <td style={{ color: `${order.orderClass}` }}>{order.orderSign}{order.token0Amount}</td>
+                    <td>{order.tokenPrice}</td>
+                  </tr>
+                )
+              })}
+            
+            </tbody>
+          </table>
+        </div>
+      )}
+        
     </div>
   )
 }
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -150,6 +150,57 @@ const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
     }
 }
 
+// my filled orders
+
+export const myFilledOrdersSelector = createSelector(account, tokens, filledOrders, (
+    account, tokens, orders
+) => {
+    if (!tokens[0] || !tokens[1]) { return }
+
+    // find our orders
+    orders = orders.filter((o) => o.user === account || o.creator === account)
+
+    // filter orders by selecred token
+    orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+    orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+    // sort orders by date desc
+    orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+
+    // decorate myFilledOrders
+    orders = decorateMyFilledOrders(orders, account, tokens)
+
+    return orders
+})
+
+const decorateMyFilledOrders = (orders, account, tokens) => {
+    return(
+        orders.map((order) => {
+            order = decorateOrder(order, tokens)
+            order = decorateMyFilledOrder(order, account, tokens)
+            return(order)
+        })
+    )
+}
+
+const decorateMyFilledOrder = (order, account, tokens) => {
+    const myOrder = order.creator === account
+
+    let orderType
+    if(myOrder) {
+        orderType = order.tokenGive === tokens[1].address ? 'buy' : 'sell'
+    } else {
+        orderType = order.tokenGive === tokens[1].address ? 'sell' : 'buy'
+    }
+
+    return({
+        ...order,
+        orderType,
+        orderClass: (orderType === 'buy' ? GREEN : RED),
+        orderSign: (orderType === 'buy' ? '+' : '-')
+    })
+}
+
 // order book
 
 export const orderBookSelector = createSelector(openOrders, tokens, (orders, tokens) => {
